dfc1f15c5b659c722ab3022a79dbcd2f2b6afc84
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-03 21:18:45 +0530

    More on Require - Creating Structures

--- app.js
+++ app.js
@@ -1,16 +1,4 @@
-var firstname = 'Jane';
+var greet = require('./greet');
 
-(function(lastname) {
-
-    var firstname = 'John';
-    console.log(firstname);
-    console.log(lastname);
-
-}('Doe'));
-
-console.log(firstname);
-
-//The variables inside the invoked functions are completely seperate and 
-//do not affect anything which is outside.
-
-// This is a n imiditaely invoked function expression and that is why we use it.
+greet.english();
+greet.spanish();
