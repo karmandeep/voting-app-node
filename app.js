609f1852713209f5fd9d062fe3ff4ab1d3b0765c
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-03 15:33:26 +0530

    Prototypal Inheritance and function construction

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
-var person = {
-    firstname: 'John',
-    lastname: 'Doe',
-    greet: function() {
-        console.log('Hello,' + this.firstname + ' ' + this.lastname );
-    }
+function Person(firstname, lastname) {
+
+    this.firstname = firstname;
+    this.lastname = lastname;
+};
+
+Person.prototype.greet = function() {
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
 };
 
 
-person.greet();
+var john = new Person('John' , 'Doe');
+john.greet();
 
-console.log(person['firstname']);
+var jane = new Person('Jane' , 'Doe');
+jane.greet();
+
+//To see How the Prototype looks like
+console.log(john.__proto__);
+console.log(jane.__proto__);
+
+console.log(john.__proto__ === jane.__proto__);
