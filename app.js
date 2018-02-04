22522e15e009c94bcace19ac43dfcb0b9b6cda6b
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-04 21:29:48 +0530

    Tried but didn't work ES6

--- app.js
+++ app.js
@@ -1,8 +1,2 @@
-//If its a Custom Module the use require('./util') -> Saying that it is LOCAL
-var util = require('util');
-//util is a NODE NATIVE MODULE
-
-var name = 'Karmandeep';
-var greeting = util.format('Hello, %s', name);
-
-util.log(greeting);
+import * as greetr from 'greet';
+greetr.greet(); 
