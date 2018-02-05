07d9b3faae6db4efd560188109cd3dec30bfe96c
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-05 17:43:42 +0530

    Events First Class Functions

--- app.js
+++ app.js
@@ -1,2 +1,32 @@
-import * as greetr from 'greet';
-greetr.greet(); 
+// object properties and methods
+
+var obj = {
+    greet: 'Hello'
+}
+
+console.log(obj.greet);
+
+console.log(obj['greet']);
+
+var prop = 'greet';
+
+console.log(obj[prop]);
+
+//Functions and Arrays
+var arr = [];
+
+arr.push(function(){
+    console.log('Hello world 1');
+});
+
+arr.push(function(){
+    console.log('Hello world 2');
+});
+
+arr.push(function(){
+    console.log('Hello world 3');
+});
+
+arr.forEach(function(item) {
+    item();
+});
