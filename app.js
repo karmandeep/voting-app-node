b66b664ce4e02342c7f561520ab61a47b0543b03
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-06 19:27:13 +0530

    Call and Apply in Javascript

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-var name = "John Doe";
+var obj = {
+    name: 'John Doe',
+    greet: function(param) {
+        console.log(`Hello ${ this.name }`);
+    }
+}
 
-var greet = 'Hello ' + name;
+obj.greet();
+//Passing an object to this function via .call and what that really means is
+//this object would become the this keyword.
+//So this function would run as normal, where-ever i see the the this keyword
+//Pointing to the parent object
 
-//With ES6 there is something called template literals.
-
-//Template Literals make it easier to build string, to concatinate strings together
-
-//Template Literal
-//It let me insert javascript code without having to stop the string and use
-//Pluses to concatinate and everything else.
+//It will instill point to th eobject that i passed in via call.
+//Some essentially borrowing this function and giving it my own object to service this
+//keyword.
 
 
-//This is a Lot faster and easier and less prone to mistakes.
+obj.greet.call({ name: 'Jane Doe' });
 
-var greet2 = `Hello ${ name }`; 
-//We can't do this in any browser, if we want to do this in any browser
-//Because it may not be supported.
-
-//But if we want to do this in the browser, use something like Babel.
-
-console.log(greet);
-console.log(greet2);
+obj.greet.apply({ name: 'Jane Doe' });
