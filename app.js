92c03d0f7219c1d152d2cb1bfce122e0968a578f
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-06 19:16:20 +0530

    Template Literals

--- app.js
+++ app.js
@@ -14,7 +14,10 @@ var greet = 'Hello ' + name;
 //This is a Lot faster and easier and less prone to mistakes.
 
 var greet2 = `Hello ${ name }`; 
+//We can't do this in any browser, if we want to do this in any browser
+//Because it may not be supported.
 
+//But if we want to do this in the browser, use something like Babel.
 
 console.log(greet);
 console.log(greet2);
