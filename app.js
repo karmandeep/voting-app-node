194efb4663a98ef6e7bd5b12c71bc7c5168f0fcf
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-08 16:23:13 +0530

    Class is a Syntactic Sugar in Node

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-var util = require('util');
+//This is to Follow Standard.
+'use strict';
+//Class is just a Suntactic Sugar .i.e its just a Function.
+//Class Person
+class Person {
+    constructor(firstname, lastname) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+    }
 
-function Person() {
-    this.firstname = 'John';
-    this.lastname = 'Doe';
+    greet() {
+        console.log('Hello ' + this.firstname + ' ' + this.lastname);        
+    }
 }
+//Don't use Concepts and patterns from other programming languages into classes.
 
-Person.prototype.greet = function() {
-    console.log('Hello ' + this.firstname + ' ' + this.lastname);
-}
 
-function Policeman() {
-    Person.call(this);
-    this.badgenumber = '1234';
-}
+//Using Classes is Easier and Cleaner looking then to setup our objects.
+var john = new Person('John' , 'Doe');
+john.greet();
 
-util.inherits(Policeman, Person);
-var officer = new Policeman();
-officer.greet(); 
+var jane = new Person('Jane' , 'Doe');
+john.greet();
+
+console.log(john.__proto__);
+console.log(john.__proto__);
+console.log(john.__proto__ === jane.__proto__ );
+
+//This is New and Clean, if you know what you are doing or if you understand
+//javascript objects, protoype chains and prototype objects.
+// Inhertance work, this can be a ver nice way syntactically.
