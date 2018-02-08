10fc789174f7d7d3c903dd20693cc5bdc795cfb8
Karmandeep Singh <karmandeep.singh@gmail.com>
2018-02-08 13:31:49 +0530

    Util.inherits - 2

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
-var EventEmitter = require('events');
 var util = require('util');
 
-function Greetr() {
-    EventEmitter.call(this);
-    this.greeting = 'Hello world!';
+function Person() {
+    this.firstname = 'John';
+    this.lastname = 'Doe';
 }
 
-util.inherits(Greetr, EventEmitter);
-
-Greetr.prototype.greet = function(data) {
-    console.log(this.greeting + ': ' + data);
-    this.emit('greet' , data);
+Person.prototype.greet = function() {
+    console.log('Hello ' + this.firstname + ' ' + this.lastname);
 }
 
-//var greeter1 = new Greetr();
-console.log(Greetr.greeting);
+function Policeman() {
+    Person.call(this);
+    this.badgenumber = '1234';
+}
+
+util.inherits(Policeman, Person);
+var officer = new Policeman();
+officer.greet(); 
